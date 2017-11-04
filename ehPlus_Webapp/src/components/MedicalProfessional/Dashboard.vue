a5127e0f6c7857586cff38715edd9b5130c09946
Lavhe <mulavhe@gmail.com>
2017-11-04 03:07:37 +0200

    WEB - Connected with firebase

---
+++ ehPlus_Webapp/src/components/MedicalProfessional/Dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase';
+
+export default {
+  name: 'MedicalprofessionalDashboard',
+  data () {
+    return {
+      msg: 'Medical'
+    }
+  },
+  mounted() {
+    //do something after mounting vue instance
+    firebase.auth().signInWithEmailAndPassword("mulavhe@gmail.com","12s3456").then(
+      function(user){
+          console.log(user.email);
+      },function(err){
+          alert(err);
+      }
+    );
+  }
+
+}
+</script>
